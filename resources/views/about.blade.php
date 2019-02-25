c9cc54fa8f7e073d4a7922268e5688e4679b9783
Philip Andes <philip__andes@hotmail.nl>
2019-02-25 16:26:55 +0100

    made different pages and nav between

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+
+@section('title', 'About Us')
+
+@section('content')
+
+<h1> About us </h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae explicabo ab hic consectetur perspiciatis voluptatum ratione, sapiente iusto, corrupti repellat iure. Consequuntur soluta, nulla rem blanditiis consectetur architecto dolore sint!</p>
+
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+
+@section('title', 'Contact Us')
+
+
+@section('content')
+
+    <h1>Contact form</h1>
+
+@endsection
